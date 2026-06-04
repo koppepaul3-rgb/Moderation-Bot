8fdfe0b1b6be928935c6b92aaaf4f1c821f966e5
koppepaul3-rgb <koppepaul3@gmail.com>
2026-06-04 20:12:17 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -301,7 +301,7 @@ class TitanBot extends Client {
 
   async registerCommands() {
     try {
-      await registerSlashCommands(this, this.config.bot.guildId);
+      await registerSlashCommands(this); // ◄ HIER WURDE ES GEÄNDERT
     } catch (error) {
       logger.error('Error registering commands:', error);
     }

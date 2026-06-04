2497bed5730127023b60ff0b3d19f3f0f174a515
koppepaul3-rgb <koppepaul3@gmail.com>
2026-06-04 16:12:37 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made by: chief_sukuna whit 💙 for other communitys."
+        name: "Made by: chief_sukuna whit 💙 for other communitys.",
         // Activity type number (0 = Playing).
         type: 0, 
       },

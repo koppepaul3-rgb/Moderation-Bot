c167a11a3a05a42925f752981cb64bcca1c7ef78
koppepaul3-rgb <koppepaul3@gmail.com>
2026-06-04 20:31:52 +0200

    Update application.js

--- src/config/application.js
+++ src/config/application.js
@@ -28,7 +28,7 @@ const appConfig = {
     ...botConfig,
     token: process.env.DISCORD_TOKEN || process.env.TOKEN,
     clientId: process.env.CLIENT_ID,
-    guildId: process.env.GUILD_ID,
+    guildId: "global",
 
     shop: {
       ...botConfig.shop,
